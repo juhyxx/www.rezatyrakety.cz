51b2d5c5bf613ba8c2f090b56ee15cfb1cc8c873
juhyxx <juhy@rezatyrakety.cz>
2014-07-19 12:40:07 +0200

    Design

--- app/scripts/controllers/Concerts.js
+++ app/scripts/controllers/Concerts.js
@@ -8,6 +8,18 @@
  * Controller of the rr
  */
 angular.module('rr')
-  .controller('Concerts', function ($scope) {
-    
-  });
+	.controller('Concerts', [
+		'$scope', '$http', '$rootScope',
+		
+		function($scope, $http, $rootScope) {
+			$http({
+				method: 'GET',
+				url: 'data.json'
+			}).success(function(data, status, headers, config) {
+				$rootScope.koncerty = data.result.koncerts;
+				$scope.odehraneKoncerty = data.result.oldkoncerts;
+			}).error(function(data, status, headers, config) {
+				window.console.error('Loading data error');
+			});
+		}
+	]);
