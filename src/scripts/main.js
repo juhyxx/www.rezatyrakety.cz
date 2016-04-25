e8e1de27b33e1bb7645d09565a4ccd23c3726551
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-04-25 17:14:49 +0200

    New design

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,51 @@
-$(function () {
+$(function() {
 
-	$('header, article').css({
-		'min-height': window.innerHeight
-	});
+  $("nav a ").click(function() {
+    var id = $(this).attr('href'),
+      left = $(id).offset().left;
 
-	$(window).resize(function () {
-		$('header, article').css({
-			'min-height': window.innerHeight
-		});
-		$('header').css({
-			'height': window.innerHeight
-		});
-	});
 
-	var messages = [
-			'Bacha!',
-			'Vy! Ano Vy!',
-			'Míříme přímo na vás!',
-			'Ну  Поехали!',
-			'Naše rez do Vašich uší...',
-			'...Vaše uši do naší sbírky',
-			'Bigbít s plochou drahou letu',
-			'Žádná raketa není dost zrezlá',
-			'Tavíme rez na muziku!',
-			'Odpalovací rampa bigbítu',
-			'První kosmický bigbít',
-			'Poslední kosmický bigbít',
-			'Bigbít s jadernou hlavicí',
-			'Jsme pohrobci studené války',
-			'Rezatá raketa díru do světa neudělá',
-			'Hudba z třetího šutru od slunce',
-		],
-		counter = 0;
+    $('body').animate({
+      scrollLeft: left
+    }, left / 2);
+  });
 
-	setInterval(function () {
-		$('#message').html(messages[counter]);
-		counter = counter > messages.length ? 0 : counter + 1;
-	}, 2000);
+  /*$('header, article').css({
+  	'min-height': window.innerHeight
+  });*/
+
+  /*$(window).resize(function() {
+    $('header, article').css({
+      'min-height': window.innerHeight
+    });
+    $('header').css({
+      'height': window.innerHeight
+    });
+});*/
+
+  var messages = [
+      'Bacha!',
+      'Vy! Ano Vy!',
+      'Míříme přímo na vás!',
+      'Ну  Поехали!',
+      'Naše rez do Vašich uší...',
+      '...Vaše uši do naší sbírky',
+      'Bigbít s plochou drahou letu',
+      'Žádná raketa není dost zrezlá',
+      'Tavíme rez na muziku!',
+      'Odpalovací rampa bigbítu',
+      'První kosmický bigbít',
+      'Poslední kosmický bigbít',
+      'Bigbít s jadernou hlavicí',
+      'Jsme pohrobci studené války',
+      'Rezatá raketa díru do světa neudělá',
+      'Hudba z třetího šutru od slunce',
+    ],
+    counter = 0;
+
+    /*setInterval(function() {
+      $('#message').html(messages[counter]);
+      counter = counter > messages.length ? 0 : counter + 1;
+}, 2000);*/
 
 });
--- src/scripts/photos.js
+++ src/scripts/photos.js
@@ -1,51 +1,51 @@
-$(function () {
-	setTimeout(function () {
-		$('body').append('<script src="http://picasaweb.google.com/data/feed/api/user/juhyxx/albumid/5252817473210531713?kind=photo&alt=json-in-script&thumbsize=243c&callback=setData&fields=entry(summary,gphoto:timestamp , media:group/media:thumbnail)"><\/script>');
-	}, 100);
+$(function() {
+  setTimeout(function() {
+    //	$('body').append('<script src="http://picasaweb.google.com/data/feed/api/user/juhyxx/albumid/5252817473210531713?kind=photo&alt=json-in-script&thumbsize=243c&callback=setData&fields=entry(summary,gphoto:timestamp , media:group/media:thumbnail)"><\/script>');
+  }, 100);
 
 });
 
 function renderPhoto(photo, photos) {
-	var rnd = Math.round(Math.random() * photos.length),
-		item = photos[rnd];
+  var rnd = Math.round(Math.random() * photos.length),
+    item = photos[rnd];
 
-	try {
-		var date = new Date(parseInt(item['gphoto$timestamp']['$t'], 10));
+  try {
+    var date = new Date(parseInt(item['gphoto$timestamp']['$t'], 10));
 
-		$(photo).addClass('loading');
-		$('img', photo).attr('src', item['media$group']['media$thumbnail'][0].url);
-		$('i', photo).html(item['summary']['$t']);
-		$('span', photo).html((date.getDay() + 1) + '.' + (date.getMonth() + 1) + '. ' + date.getFullYear());
-	} catch (e) {
-		console.error('Unable to render item : ', item, e);
-	}
+    $(photo).addClass('loading');
+    $('img', photo).attr('src', item['media$group']['media$thumbnail'][0].url);
+    $('i', photo).html(item['summary']['$t']);
+    $('span', photo).html((date.getDay() + 1) + '.' + (date.getMonth() + 1) + '. ' + date.getFullYear());
+  } catch (e) {
+    console.error('Unable to render item : ', item, e);
+  }
 }
 
 function renderPhotos(photos) {
-	$('.photo').each(function (index, photoEl) {
-		setTimeout(function () {
-			$('img', photoEl).load(function () {
-				var parent = $(this).parent('.photo');
-				parent.removeClass('loading');
-				setTimeout(function () {
-					parent.addClass('loading');
-					setTimeout(function () {
-						renderPhoto(photoEl, photos);
-					}, 3000);
-				}, 10000);
-			});
-			renderPhoto(photoEl, photos);
-		}, index * 3000);
-	});
+  $('.photo').each(function(index, photoEl) {
+    setTimeout(function() {
+      $('img', photoEl).load(function() {
+        var parent = $(this).parent('.photo');
+        parent.removeClass('loading');
+        setTimeout(function() {
+          parent.addClass('loading');
+          setTimeout(function() {
+            renderPhoto(photoEl, photos);
+          }, 3000);
+        }, 10000);
+      });
+      renderPhoto(photoEl, photos);
+    }, index * 3000);
+  });
 }
 
 function setData(data) {
-	var imgMarkup = '<div class="photo loading"><img><i></i><span></span></div>';
+  var imgMarkup = '<div class="photo loading"><img><i></i><span></span></div>';
 
-	$('article:not(article:first-child)>div')
-		.append(imgMarkup)
-		.prepend(imgMarkup);
+  $('article:not(article:first-child)>div')
+    .append(imgMarkup)
+    .prepend(imgMarkup);
 
-	renderPhotos(data.feed.entry, true);
+  renderPhotos(data.feed.entry, true);
 
 }
