dbd427ab4a6c0fc4b39cdd758ea6b14a63530101
Miroslav Juhy Juhos <juhyxx@gmail.com>
2025-04-11 18:42:50 +0200

    deploy

--- demo/index.php
+++ demo/index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name= "viewport" content= "width = device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <style>
+            
             body {
                 font-family: sans-serif;
                 background: #9b1915;
