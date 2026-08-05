278b7a7d07dbed6226f34b7a2c58aac97d462eb7
Miroslav Juhy Juhos <juhyxx@gmail.com>
2026-08-05 21:06:32 +0200

    vyuka

---
+++ vyuka/script.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  // Scroll-reveal animations
+  var revealEls = document.querySelectorAll('[data-reveal]');
+  if ('IntersectionObserver' in window && revealEls.length) {
+    var observer = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.15, rootMargin: '0px 0px -40px 0px' });
+
+    revealEls.forEach(function (el) { observer.observe(el); });
+  } else {
+    revealEls.forEach(function (el) { el.classList.add('is-visible'); });
+  }
+})();
